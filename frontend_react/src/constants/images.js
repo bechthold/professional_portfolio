b749456f0977b0ae3ed90561ca3399c4d917ee8f
Igor Bechthold <igor.bechthold@gmail.com>
2023-06-14 12:52:02 +0200

    add navbar and header section

---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,15 @@
+import logo from '../assets/logo.png';
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import java from '../assets/java.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+
+export default {
+  logo,
+  profile,
+  circle,
+  java,
+  node,
+  react
+}
